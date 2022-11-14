0f6a8a33f1a77d7b1f8be90d9ee804e18ee63e8d
Nabnit Jha <nabnit.jha@sofossoftwares.com>
2022-11-14 22:07:24 +0545

    improve code

--- src/main.js
+++ src/main.js
@@ -1,34 +1,26 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-
 import axios from "axios";
-
+import connectAPI from './connectAPI'
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
-
-// Import Bootstrap and BootstrapVue CSS files (order is important)
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
-
 import Fragment from "vue-fragment";
-Vue.use(Fragment.Plugin);
-
-// Make BootstrapVue available throughout your project
-Vue.use(BootstrapVue);
-// Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin);
-
 import { Plugin } from "vue-fragment";
-Vue.use(Plugin);
-
 import { createPinia, PiniaVuePlugin } from "pinia";
+
 
 Vue.use(PiniaVuePlugin);
 const pinia = createPinia();
 
-// Vue.config.globalProperties.apiUrl = "http://127.0.0.1:8000";
-
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+Vue.use(Fragment.Plugin);
+Vue.use(Plugin);
 Vue.use(axios);
+Vue.mixin(connectAPI);
+
 
 new Vue({
   components: { Fragment },
