ac61966f2b0c617cb5b137a7915866d12f45e8f6
Nabnit Jha <nabnit.jha@sofossoftwares.com>
2022-11-16 00:24:43 +0545

    improve code

--- src/components/form/Student.vue
+++ src/components/form/Student.vue
@@ -190,20 +190,6 @@ export default {
     addStudent() {
       this.checkValidation()
     },
-    checkValidation() {
-      var needsValidation = document.querySelectorAll('.needs-validation')
-      Array.prototype.slice.call(needsValidation)
-        .forEach(function (form) {
-          form.addEventListener('submit', function (event) {
-            if (!form.checkValidity()) {
-              event.preventDefault()
-              event.stopPropagation()
-            }
-
-            form.classList.add('was-validated')
-          }, false)
-        })
-    }
   }
 };
 </script>
--- src/connectAPI.js
+++ src/connectAPI.js
@@ -77,6 +77,20 @@ export default {
     changeRoute(route) {
       this.$router.push(route);
     },
+    checkValidation() {
+      var needsValidation = document.querySelectorAll('.needs-validation')
+      Array.prototype.slice.call(needsValidation)
+        .forEach(function (form) {
+          form.addEventListener('submit', function (event) {
+            if (!form.checkValidity()) {
+              event.preventDefault()
+              event.stopPropagation()
+            }
+
+            form.classList.add('was-validated')
+          }, false)
+        })
+    },
   },
   mounted() {
     this.setTokenToHeader();
