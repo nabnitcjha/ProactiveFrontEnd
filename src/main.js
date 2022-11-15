07d91931fb77963d267815b9fdb9640d47901f19
Nabnit Jha <nabnit.jha@sofossoftwares.com>
2022-11-15 18:35:15 +0545

    improve code

--- src/main.js
+++ src/main.js
@@ -4,6 +4,26 @@ import router from "./router";
 import axios from "axios";
 import connectAPI from './connectAPI'
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import {
+  ValidationObserver,
+  ValidationProvider,
+  extend,
+  localize
+} from "vee-validate";
+import en from "vee-validate/dist/locale/en.json";
+import * as rules from "vee-validate/dist/rules";
+
+// Install VeeValidate rules and localization
+Object.keys(rules).forEach(rule => {
+  extend(rule, rules[rule]);
+});
+
+localize("en", en);
+
+// Install VeeValidate components globally
+Vue.component("ValidationObserver", ValidationObserver);
+Vue.component("ValidationProvider", ValidationProvider);
+
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 import Fragment from "vue-fragment";
