de385e36891396204a3ce45d385f241cdac1ec4f
Nabnit Jha <nabnit.jha@sofossoftwares.com>
2022-11-15 22:40:21 +0545

    improve code

--- src/views/form/student/AddStudent.vue
+++ src/views/form/student/AddStudent.vue
@@ -5,67 +5,73 @@
       <div class="card">
         <div class="card-body">
           <h5 class="card-title">Custom Styled Validation with Tooltips</h5>
-          <p>If your form layout allows it, you can swap the <code>.{valid|invalid}-feedback</code> classes for
-            .<code>{valid|invalid}-tooltip</code> classes to display validation feedback in a styled tooltip. Be
-            sure to have a parent with <code>position: relative</code> on it for tooltip positioning. In the
-            example below, our column classes have this already, but your project may require an alternative
-            setup. </p>
-
           <!-- Custom Styled Validation with Tooltips -->
-          <form class="row g-3 needs-validation" novalidate @submit="addStudent">
-            <div class="col-md-4 position-relative">
-              <label for="validationTooltip01" class="form-label">First name</label>
-              <input type="text" class="form-control" id="validationTooltip01" value="John" required>
-              <div class="valid-tooltip">
+
+          <form class="row g-3 needs-validation" novalidate>
+            <div class="col-md-4">
+              <label for="validationCustom01" class="form-label">First name</label>
+              <input type="text" class="form-control" id="validationCustom01" value="John" required>
+              <div class="valid-feedback">
                 Looks good!
               </div>
             </div>
-            <div class="col-md-4 position-relative">
-              <label for="validationTooltip02" class="form-label">Last name</label>
-              <input type="text" class="form-control" id="validationTooltip02" value="Doe" required>
-              <div class="valid-tooltip">
+            <div class="col-md-4">
+              <label for="validationCustom02" class="form-label">Last name</label>
+              <input type="text" class="form-control" id="validationCustom02" value="Doe" required>
+              <div class="valid-feedback">
                 Looks good!
               </div>
             </div>
-            <div class="col-md-4 position-relative">
-              <label for="validationTooltipUsername" class="form-label">Username</label>
+            <div class="col-md-4">
+              <label for="validationCustomUsername" class="form-label">Username</label>
               <div class="input-group has-validation">
-                <span class="input-group-text" id="validationTooltipUsernamePrepend">@</span>
-                <input type="text" class="form-control" id="validationTooltipUsername"
-                  aria-describedby="validationTooltipUsernamePrepend" required>
-                <div class="invalid-tooltip">
-                  Please choose a unique and valid username.
+                <span class="input-group-text" id="inputGroupPrepend">@</span>
+                <input type="text" class="form-control" id="validationCustomUsername"
+                  aria-describedby="inputGroupPrepend" required>
+                <div class="invalid-feedback">
+                  Please choose a username.
                 </div>
               </div>
             </div>
-            <div class="col-md-6 position-relative">
-              <label for="validationTooltip03" class="form-label">City</label>
-              <input type="text" class="form-control" id="validationTooltip03" required>
-              <div class="invalid-tooltip">
+            <div class="col-md-6">
+              <label for="validationCustom03" class="form-label">City</label>
+              <input type="text" class="form-control" id="validationCustom03" required>
+              <div class="invalid-feedback">
                 Please provide a valid city.
               </div>
             </div>
-            <div class="col-md-3 position-relative">
-              <label for="validationTooltip04" class="form-label">State</label>
-              <select class="form-select" id="validationTooltip04" required>
+            <div class="col-md-3">
+              <label for="validationCustom04" class="form-label">State</label>
+              <select class="form-select" id="validationCustom04" required>
                 <option selected disabled value="">Choose...</option>
                 <option>...</option>
               </select>
-              <div class="invalid-tooltip">
+              <div class="invalid-feedback">
                 Please select a valid state.
               </div>
             </div>
-            <div class="col-md-3 position-relative">
-              <label for="validationTooltip05" class="form-label">Zip</label>
-              <input type="text" class="form-control" id="validationTooltip05" required>
-              <div class="invalid-tooltip">
+            <div class="col-md-3">
+              <label for="validationCustom05" class="form-label">Zip</label>
+              <input type="text" class="form-control" id="validationCustom05" required>
+              <div class="invalid-feedback">
                 Please provide a valid zip.
               </div>
             </div>
             <div class="col-12">
-              <button class="btn btn-primary" type="submit">Submit form</button>
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
+                <label class="form-check-label" for="invalidCheck">
+                  Agree to terms and conditions
+                </label>
+                <div class="invalid-feedback">
+                  You must agree before submitting.
+                </div>
+              </div>
             </div>
-          </form><!-- End Custom Styled Validation with Tooltips -->
+            <div class="col-12">
+              <button class="btn btn-primary" type="submit" @click.stop="addStudent">Submit form</button>
+            </div>
+          </form><!-- End Custom Styled Validation -->
 
         </div>
       </div>
@@ -88,9 +94,10 @@ export default {
     };
   },
   methods: {
-
-    addStudent(e) {
-      e.preventDefault();
+    addStudent() {
+      this.checkValidation()
+    },
+    checkValidation() {
       var needsValidation = document.querySelectorAll('.needs-validation')
       Array.prototype.slice.call(needsValidation)
         .forEach(function (form) {
