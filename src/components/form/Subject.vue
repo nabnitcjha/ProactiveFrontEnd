104fe47e0d5d8b6b3d6fcd53346cdeb620a9bf29
Nabnit Jha <nabnit.jha@sofossoftwares.com>
2022-11-24 09:41:24 +0545

    improve code

--- src/components/form/Subject.vue
+++ src/components/form/Subject.vue
@@ -4,75 +4,22 @@
 
       <div class="card">
         <div class="card-body">
-          <h5 class="card-title">Add Student and Parent Information</h5>
+          <h5 class="card-title">Subject Info</h5>
+          <hr class="hr-color">
           <form class="row g-3 needs-validation" novalidate>
             <div class="col-md-4">
-              <label for="student_first_name" class="form-label">First name</label>
-              <input type="text" class="form-control" id="student_first_name" required>
-              <div class="valid-feedback">
+              <label for="subject_first_name" class="form-label"><img :src="icons.First_name" />&nbsp;First name</label>
+              <input type="text" class="form-control" id="subject_first_name" required v-model="subject.First_name">
+              <!-- <div class="valid-feedback">
                 Looks good!
-              </div>
+              </div> -->
               <div class="invalid-feedback">
                 Please choose a fist name.
               </div>
             </div>
 
-            <div class="col-md-4">
-              <label for="student_last_name" class="form-label">Last name</label>
-              <input type="text" class="form-control" id="student_last_name" required>
-              <div class="valid-feedback">
-                Looks good!
-              </div>
-              <div class="invalid-feedback">
-                Please choose a last name.
-              </div>
-            </div>
-
-            <div class="col-md-4">
-              <label for="student_phone" class="form-label">Phone</label>
-              <input type="text" class="form-control" id="student_phone" required>
-              <div class="valid-feedback">
-                Looks good!
-              </div>
-              <div class="invalid-feedback">
-                Please choose a phone.
-              </div>
-            </div>
-
-            <div class="col-md-4">
-              <label for="student_email" class="form-label">Email</label>
-              <input type="text" class="form-control" id="student_email" required>
-              <div class="valid-feedback">
-                Looks good!
-              </div>
-              <div class="invalid-feedback">
-                Please choose a email.
-              </div>
-            </div>
-
-            <div class="col-md-4">
-              <label for="Student_dob" class="form-label">DOB</label>
-              <input type="date" class="form-control" id="Student_dob" required>
-              <div class="valid-feedback">
-                Looks good!
-              </div>
-              <div class="invalid-feedback">
-                Please choose a date of birth.
-              </div>
-            </div>
-
-            <div class="col-md-4">
-              <label for="student_address" class="form-label">Address</label>
-              <input type="text" class="form-control" id="student_address" required>
-              <div class="valid-feedback">
-                Looks good!
-              </div>
-              <div class="invalid-feedback">
-                Please choose a address.
-              </div>
-            </div>
             <div class="col-12">
-              <button class="btn btn-primary" type="submit" @click.stop="addStudent">Submit form</button>
+              <button class="btn btn-success" @click.stop="addSubject">Save</button>
             </div>
           </form><!-- End Custom Styled Validation -->
 
@@ -90,30 +37,36 @@ body {
 </style>
 
 <script>
+import {
+  First_name
+} from "../../Assets/formIcons/index";
 export default {
   data() {
     return {
-
+      icons: {
+        First_name: First_name
+      },
+      subject: {
+        name: ''
+      },
     };
   },
   methods: {
-    addStudent() {
-      this.checkValidation()
+    callBack() {
+      this.save();
     },
-    checkValidation() {
-      var needsValidation = document.querySelectorAll('.needs-validation')
-      Array.prototype.slice.call(needsValidation)
-        .forEach(function (form) {
-          form.addEventListener('submit', function (event) {
-            if (!form.checkValidity()) {
-              event.preventDefault()
-              event.stopPropagation()
-            }
+    addSubject() {
+      this.checkValidation(this.callBack)
+    },
+    async save() {
+      let formData = new FormData();
 
-            form.classList.add('was-validated')
-          }, false)
-        })
-    }
+      formData.append("subject_info[name]", this.subject.name);
+      let postResponse = {};
+      let urlText = 'addSubject';
+
+      postResponse = await this.post(urlText, formData);
+    },
   }
 };
 </script>
